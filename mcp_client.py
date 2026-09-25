2c31ce21821e780c1f6cd3567025807f6ca45779
Parfait Ben-oni TEDOM TEDOM <45407147+theparadoxShin@users.noreply.github.com>
2026-09-25 03:21:37 -0600

    Add JSON parsing to read_resource method in MCPClient

--- mcp_client.py
+++ mcp_client.py
@@ -1,9 +1,11 @@
 import sys
 import asyncio
+import json
 from typing import Optional, Any
 from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
+from pydantic import AnyUrl
 
 
 class MCPClient:
@@ -63,7 +65,12 @@ class MCPClient:
 
     async def read_resource(self, uri: str) -> Any:
         # TODO: Read a resource, parse the contents and return it
-        return []
+        result = await self.session().read_resource(AnyUrl(uri))
+        resource = result.contents[0]
+        if isinstance(resource, types.TextResourceContents):
+            if resource.mimeType == "application/json":
+                return json.loads(resource.text)
+            return resource.text
 
     async def cleanup(self):
         await self._exit_stack.aclose()
